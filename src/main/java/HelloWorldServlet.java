53a692791f2accf9d25e84360b1d4e29376653d5
Andrew Lange <andrew.lange13@gmail.com>
2022-03-22 11:17:46 -0500

    adds hello world and hit counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,30 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello-world", "/", "/hello"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    private int hitCounter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+
+        hitCounter++;
+        out = response.getWriter();
+        String title = "Hit Counter";
+        String docType = "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<h2 align=\"center\">" + hitCounter + "</h2>\n" +
+                "</body></html>");
     }
+
 }
